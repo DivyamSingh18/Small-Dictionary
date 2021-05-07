7ac2c58c36dc4aaccfd6f882d121da78d23cec02
DivyamSingh <64833579+DivyamSingh18@users.noreply.github.com>
2021-05-07 11:21:36 +0530

    Add files via upload

---
+++ small dictionary.py
@@ -0,0 +1,11 @@
+dict1 = {
+        "tuple": "a data structure consisting of multiple parts.",
+        "abandon": "cease to support or look after (someone); desert.",
+        "irony": "the expression of one's meaning by using language that normally signifies the opposite, typically for humorous or emphatic effect.",
+        "savage": "violent, and uncontrolled"
+}
+
+print("Enter a Word")
+
+
+print(dict1[input()])
